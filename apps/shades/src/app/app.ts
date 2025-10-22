d84ec8db303914ba7b4984d7d85d951e2158ed44
Rafael González <rafael.gonzalez@nixs.com>
2025-10-22 15:56:57 +0200

    feat(shades): topbar and app configure the shades generation process

--- apps/shades/src/app/app.ts
+++ apps/shades/src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ShadesList } from '../components/shadesList/shadesList';
+import { Component, computed, model, signal } from '@angular/core';
+import { Format, ShadesList } from '../components/shadesList/shadesList';
 import { Topbar } from '../components/topbar/topbar';
 
 @Component({
@@ -10,4 +10,24 @@ import { Topbar } from '../components/topbar/topbar';
 })
 export class App {
   protected title = 'color-shades-generator';
+  colors = signal([] as string[]);
+  steps = 5
+  format: Format = 'hex'
+  output = 'css'
+
+  addColor = () => {    
+    const upd = [...this.colors()]
+    upd.push(crypto.randomUUID())
+    this.colors.set(upd)
+  }
+
+  removeColor = (index: number) => {
+    const upd = [...this.colors()]
+    upd.splice(index, 1)
+    this.colors.set(upd)
+  }
+
+  showOutput = () => {
+    alert('output!')
+  }
 }
--- apps/shades/src/components/topbar/topbar.ts
+++ apps/shades/src/components/topbar/topbar.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, model, ModelSignal, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColorSketchModule } from 'ngx-color/sketch'
+import { Format } from '../shadesList/shadesList';
 
 @Component({
+  imports: [ColorSketchModule, FormsModule],
   selector: 'app-topbar',
-  imports: [],
   templateUrl: './topbar.html',
   styleUrl: './topbar.css',
 })
-export class Topbar {}
+export class Topbar {
+  steps = model<number>()
+  format = model<Format>()
+  output = model<string>()
+  addColor = output<void>()
+  showOutput = output<void>()
+}
