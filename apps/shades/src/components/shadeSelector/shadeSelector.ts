64bcf11252632417b0d8efa9420999436b36f0ce
Rafael González <rafael.gonzalez@nixs.com>
2025-10-27 11:38:53 +0100

    fix(shades): add color picker and icons to the shade selector

--- apps/shades/src/components/shadeSelector/shadeSelector.ts
+++ apps/shades/src/components/shadeSelector/shadeSelector.ts
@@ -2,10 +2,14 @@ import { Component, computed, model, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import ColorNamer from 'color-namer';
 import { TitleCasePipe } from '@angular/common'
+import { Button } from '../button/button';
+import { ColorSketchModule } from 'ngx-color/sketch';
+import { ColorChromeModule } from 'ngx-color/chrome';
+import { ColorEvent } from 'ngx-color';
 
 @Component({
   selector: 'app-shade-selector',
-  imports: [FormsModule, TitleCasePipe],
+  imports: [FormsModule, TitleCasePipe, Button, ColorSketchModule, ColorChromeModule],
   templateUrl: './shadeSelector.html',
   styleUrl: './shadeSelector.css',
 })
@@ -13,4 +17,14 @@ export class ShadeSelector {
   code = model<string>('')
   name = computed(() => ColorNamer(this.code()).pantone[0].name)
   onRemove = output<void>()
+  showPicker = false
+
+  togglePicker(state: boolean) {
+    this.showPicker = state
+  }
+
+  handleColorChange($event: ColorEvent) {
+    console.log($event.color);
+    this.code.set($event.color.hex)
+  }
 }
