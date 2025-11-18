8fedffd62d8a17753d1e570fb0795e2591a629c8
Rafael González <rafael.gonzalez@nixs.com>
2025-11-18 11:13:58 +0100

    fix(pomodoro): add controls button component

---
+++ apps/pomodoro/src/components/button/btn.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+const {
+  disable,
+  onClick
+} = defineProps<{
+  disable?: boolean
+  onClick: () => void
+}>()
+</script>
+
+<template>
+  <button class="btn" :disable="disable" @click="onClick">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 0.5rem;
+  line-height: 1rem;
+  aspect-ratio: 1;
+  border-radius: 2rem;
+  width: 2rem;
+  height: 2rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 150ms ease-in-out;
+  background: white;
+  color: rgba(0, 0, 0, 0.45);
+}
+
+.btn:hover {
+  color:
+    white;
+  background:
+    hsl(168, 100%, 40%);
+  box-shadow: 0 0.25rem 0.5rem 0 rgba(0, 0, 0, 0.1);
+  transform: translateY(-10%) scale(1.1);
+}
+</style>
