717e54c17ccf31f01d245f97a252a2953d5eb608
Rafael González <rafael.gonzalez@nixs.com>
2025-11-19 18:05:14 +0100

    fix(pomodoro): use hooks for the timer. improve code readability

--- apps/pomodoro/src/components/timer/timer.vue
+++ apps/pomodoro/src/components/timer/timer.vue
@@ -1,9 +1,187 @@
 <script setup lang="ts">
-// defineProps<{}>()
+import {
+  IconArrowBackUpDouble,
+  IconBellRingingFilled,
+  IconBellXFilled,
+  IconPlayerPauseFilled,
+  IconPlayerPlayFilled,
+  IconPlayerSkipBackFilled,
+  IconPlayerSkipForwardFilled,
+  IconPlayerStopFilled,
+} from '@tabler/icons-vue'
+import { onMounted, ref, watch } from 'vue'
+import calm from '../../../assets/calm.wav'
+import chord from '../../../assets/chord.wav'
+import chord2 from '../../../assets/chord2.wav'
+import { useQueue } from '../../hooks/useQueue'
+import { useRounds } from '../../hooks/useRounds'
+import { useTimer } from '../../hooks/useTimer'
+import { Timer, TimerType } from '../../types'
+import btn from '../button/btn.vue'
+import dial from '../dial/dial.vue'
+
+const sounds = ref({
+  [TimerType.FOCUS]: new Audio(chord),
+  [TimerType.BREAK]: new Audio(chord2),
+  stop: new Audio(calm)
+})
+
+const muted = ref(false)
+const toggleMute = () => muted.value = !muted.value
+
+const rounds = useRounds()
+const [timer, { stop, play, set, pause }] = useTimer({
+  onFinished: () => next()
+})
+
+const [queue, { enqueue, next, previous, goTo }] = useQueue<Timer>({
+  onEnqueue: ([first]) => set(first.seconds),
+  onFinish: ({ item }) => {
+    stop()
+    if (!muted.value) item.sound.play()
+  },
+  onChange: ({ item, stepSize }) => {
+    set(item.seconds)
+    if (Math.abs(stepSize) === 1) play()
+    if (!muted.value) {
+      item.sound.volume = 0.2
+      item.sound.play()
+    }
+  }
+})
+
+onMounted(() => {
+  enqueue(rounds)
+})
+
+watch(queue, () => {
+  console.log(queue.value.current.seconds)
+})
+
 </script>
 
 <template>
-  <p>Welcome to Timer!</p>
+  <div v-if="queue.current" class="timer">
+    <dial :class="`status-${timer.status}`" :timer="queue.current" color="hsl(162, 85%, 50%)" :status="timer.status" />
+    <div class="label">{{ queue.current.label }}</div>
+    <div :class="`time status-${timer.status}`" style="--color: hsl(162, 85%, 50%);">
+      {{ timer.minutes }}:{{ timer.seconds }}
+    </div>
+    <div class="index">({{ queue.current.index }} / {{ queue.length }})</div>
+    <btn v-if="timer.status === 'play'" class="play" @click="pause">
+      <IconPlayerPauseFilled />
+    </btn>
+    <btn v-else class="play" @click="play">
+      <IconPlayerPlayFilled />
+    </btn>
+    <btn v-if="timer.status !== 'stop'" class="stop" @click="stop">
+      <IconPlayerStopFilled />
+    </btn>
+    <btn v-else :disable="queue.index === 0" class="stop" @click="() => goTo(0)">
+      <IconArrowBackUpDouble />
+    </btn>
+    <btn :disable="queue.index >= queue.length - 1" class="next" @click="next">
+      <IconPlayerSkipForwardFilled />
+    </btn>
+    <btn :disable="queue.index === 0" class="prev" @click="previous">
+      <IconPlayerSkipBackFilled />
+    </btn>
+    <btn class="vol" @click="toggleMute">
+      <IconBellXFilled v-if="muted" />
+      <IconBellRingingFilled v-else />
+    </btn>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.timer {
+  padding: 1rem;
+  /* border-radius: 1rem; */
+  /* box-shadow: 0 0 1rem 0.5rem rgba(0, 0, 0, 0.15); */
+  width: 100%;
+  max-width: 300px;
+  margin: 0 auto;
+  position: relative;
+  text-align: center;
+
+  display: grid;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  gap: 2rem;
+  grid-template:
+    'label  label  label    label  label' 1rem
+    'dial   dial   dial     dial    dial' auto
+    'stop   prev   play     next     vol' 1rem
+    'index  index  index    index  index' 1rem
+    / 1fr 1fr 1fr 1fr 1fr;
+}
+
+.time {
+  position: absolute;
+  top: calc(50% - 1rem);
+  left: 50%;
+  transform: translate(-50%, -50%);
+  vertical-align: middle;
+  font-size: 24px;
+  line-height: 1em;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.15);
+
+  &.status- {
+    &play {
+      color: var(--color);
+    }
+  }
+}
+
+.dial {
+  grid-area: dial;
+}
+
+.label {
+  grid-area: label;
+  font-size: 14px;
+  font-weight: bold;
+  opacity: 0.35;
+}
+
+.index {
+  grid-area: index;
+  font-size: 12px;
+  font-weight: bold;
+  opacity: 0.35;
+}
+
+/* .controls {
+  grid-area: ctrl;
+
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+} */
+
+.play {
+  grid-area: play;
+}
+
+.reset {
+  grid-area: reset;
+}
+
+.stop {
+  grid-area: stop;
+}
+
+.vol {
+  grid-area: vol;
+}
+
+.next {
+  grid-area: next;
+}
+
+.prev {
+  grid-area: prev;
+}
+</style>
